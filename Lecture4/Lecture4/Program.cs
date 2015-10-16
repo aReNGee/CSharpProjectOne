096b1fcf26713ad2b6ecad765f38c15234eee5b6
Ivan Boyko <ivan.h.boyko@gmail.com>
2015-10-16 11:11:42 -0400

    First Update of Lecture4
    Added prompt for user imput (PlayerAlias)

--- Lecture4/Lecture4/Program.cs
+++ Lecture4/Lecture4/Program.cs
@@ -10,6 +10,14 @@ namespace Lecture4
     {
         static void Main(string[] args)
         {
+            
+            string PlayerAlias; //myPlayerName
+            Console.WriteLine("Welcome to the world of Swordbreaker! Swords don't last long here, I highly");
+            Console.WriteLine("recommend you use axes instead.");
+            Console.WriteLine("Names are a valuable commodity here. Please enter the alias you'd like to use.");
+            PlayerAlias = Console.ReadLine();
+            Console.WriteLine("You have chosen the alias {0}, is that correct?", PlayerAlias);
+            Console.Read();
         }
     }
 }
