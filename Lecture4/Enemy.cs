5a5b59a0a99abb5b029a88a7529dbf0ac07c39fd
Ivan Boyko <ivan.h.boyko@gmail.com>
2015-10-16 13:18:04 -0400

    Update - End of Lecture
    Added new class - Enemy.cs

---
+++ Lecture4/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lecture4
+{
+    public class Enemy
+    {
+        //type // name // value
+        string name;
+        int weaponCount;
+        bool isAlive;
+        int currentState;
+        int bodyParts;
+        // getter and setter //accessor and mutator
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        
+
+        public Enemy()
+        {
+            name = "Slime";
+            weaponCount = 5;
+            isAlive = true;
+            currentState = 2;
+            bodyParts = 4000;
+        }
+
+        public Enemy(string enemyName, int weapon, bool alive, int state, int soManyBodyParts)
+        {
+            name = enemyName;
+            weaponCount = weapon;
+            isAlive = alive;
+            currentState = state;
+            bodyParts = soManyBodyParts;
+        }
+
+        public void slimeAttack() //enemyAttack
+        {
+            Console.WriteLine("THE SLIME DRAWS NEAR! DEFEND YOURSELF!");
+        }
+    }
+}
--- Lecture4/Lecture4/Program.cs
+++ Lecture4/Lecture4/Program.cs
@@ -10,13 +10,26 @@ namespace Lecture4
     {
         static void Main(string[] args)
         {
-            
+            Enemy firstEnemy = new Enemy();
+            Enemy secondEnemy = new Enemy("Miguel", 4, true, 2, 6000);
+            firstEnemy.slimeAttack();
+
             string PlayerAlias; //myPlayerName
+            string yesno; //myDecision
             Console.WriteLine("Welcome to the world of Swordbreaker! Swords don't last long here, I highly");
             Console.WriteLine("recommend you use axes instead.");
             Console.WriteLine("Names are a valuable commodity here. Please enter the alias you'd like to use.");
             PlayerAlias = Console.ReadLine();
             Console.WriteLine("You have chosen the alias {0}, is that correct?", PlayerAlias);
+            yesno = Console.ReadLine();
+            if (yesno == "y")
+            {
+                Console.WriteLine("Great! You have a great future ahead of you, {0}.", PlayerAlias);
+            }
+            else
+            {
+                Console.WriteLine("Guess what? I don't even care. Your name is {0} now, buddy.", PlayerAlias);
+            }
             Console.Read();
         }
     }
